351c859e89b9d767cf7f7a8cf2369322e5055746
uDay-selise <saiful.uday@selisegroup.com>
2024-12-11 16:45:18 +0600

    change env value

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_BACKEND_URL: "https://dev-api.seliseblocks.com",
-    NEXT_PUBLIC_X_BLOCKS_KEY: "93c21ea21083453d93dbcbd2fee69aab",
-  },
+  // env: {
+  //   NEXT_PUBLIC_BACKEND_URL: "https://blocks-api.seliselocal.com",
+  //   NEXT_PUBLIC_X_BLOCKS_KEY: "d2e1e605eeed417d86f6cb17f246d2cc",
+  // },
 };
 
 export default nextConfig;
