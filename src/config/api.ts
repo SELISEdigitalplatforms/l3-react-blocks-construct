5d4df463f544e002c238798bf9b39176854c3a22
afiapreety <afia.preety@selisegroup.com>
2025-01-26 16:35:11 +0600

    feat: changed project keys

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 const API_CONFIG = {
   baseUrl: process.env.REACT_APP_PUBLIC_BACKEND_URL || 'https://dev-api.seliseblocks.com',
-  blocksKey: process.env.REACT_APP_PUBLIC_X_BLOCKS_KEY || 'cf18dc87904c4e1485639242cda4a026',
+  blocksKey: process.env.REACT_APP_PUBLIC_X_BLOCKS_KEY || 'a7842ae0f663408bb67cc13ab8c6dc50',
   auth: {
     token: '/authentication/v1/oauth/token',
   },
