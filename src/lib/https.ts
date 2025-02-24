16dc6bb9fab5d8013d2a1788bb3107ea9fc5d7b0
afiapreety <afia.preety@selisegroup.com>
2025-02-24 22:04:38 +0600

    feat: cpactha condition updated on signin page

--- src/lib/https.ts
+++ src/lib/https.ts
@@ -74,13 +74,13 @@ export const clients: Https = {
 
     const config: RequestInit = {
       method,
-      ...(process.env.REACT_APP_COOKIE_ENABLED === 'false' && {
+      ...(process.env.REACT_APP_COOKIE_ENABLED === 'true' && {
         credentials: 'include',
       }),
       headers: new Headers({
         'Content-Type': 'application/json',
         'x-blocks-key': BLOCKS_KEY,
-        ...(process.env.REACT_APP_COOKIE_ENABLED === 'false' &&
+        ...(process.env.REACT_APP_COOKIE_ENABLED === 'true' &&
           useAuthStore.getState().accessToken && {
             Authorization: `bearer ${useAuthStore.getState().accessToken}`,
           }),
