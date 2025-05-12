0efd3b02aa1e9007afe79c5f2f2092fa6d0cd777
afiapreety <afia.preety@selisegroup.com>
2025-05-12 17:01:18 +0600

    fix: testing

--- src/pages/error/service-unavailable/service-unavailable.tsx
+++ src/pages/error/service-unavailable/service-unavailable.tsx
@@ -2,11 +2,12 @@ import { Button } from 'components/ui/button';
 import { RefreshCcw } from 'lucide-react';
 import temporaryUnavailable from 'assets/images/unavailable.svg';
 
+/*testing from own branch*/
 export default function ServiceUnavailable() {
   return (
     <div className="flex justify-center items-center w-full">
       <div className="flex flex-col gap-12">
-        <img src={temporaryUnavailable} alt=''/>
+        <img src={temporaryUnavailable} alt="" />
         <div className="flex flex-col items-center">
           <h1 className="text-high-emphasis font-bold text-[32px] leading-[48px]">
             This page is temporarily unavailable.
@@ -14,7 +15,7 @@ export default function ServiceUnavailable() {
           <p className="mt-3 mb-6 text-medium-emphasis font-semibold text-2xl">
             Scheduled maintenance is in progress. Everything will be back to normal soon.
           </p>
-          <Button className='font-bold text-sm'>
+          <Button className="font-bold text-sm">
             Reload Page
             <RefreshCcw />
           </Button>
