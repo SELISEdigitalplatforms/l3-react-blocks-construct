07c5d4673e323323ee02a5eeac7e5f439691d8e8
armanrahman1999 <arman.rahman@selisegroup.com>
2025-09-08 18:00:38 +0600

    fix: removed the components that dont work and also changed the logo

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,10 +1,11 @@
 import { addons } from '@storybook/addons';
 import { themes } from '@storybook/theming';
+import './manager.css';
 
 addons.setConfig({
   theme: {
     ...themes.dark,
     brandTitle: 'Selise Blocks',
-    brandImage: './selise_logo_small.svg',
+    brandImage: './selise-logo.svg',
   },
 });
