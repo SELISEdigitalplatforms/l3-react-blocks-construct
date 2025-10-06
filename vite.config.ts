0acd9c73bb3e4526fb31fe87b5c5994b96044a4b
UDay <uday.dev.bd@gmail.com>
2025-10-06 12:45:24 +0600

    fix: resolve vite config for test

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -62,7 +63,8 @@ export default defineConfig({
       provider: 'v8',
       reporter: ['lcov', 'text', 'html'],
       reportsDirectory: 'coverage',
-      include: ['src/**/*.{ts,tsx}'],
+      include: ['src/**/*.spec.{ts,tsx}'],
+
       exclude: [
         'src/**/*.spec.{ts,tsx}',
         'src/**/*.test.{ts,tsx}',
