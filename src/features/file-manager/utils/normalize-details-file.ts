56af8528eb60b0ab3a212606367c451156db6c9c
Sangay Thinley <59992112+sangayt1997@users.noreply.github.com>
2025-10-21 11:04:33 +0600

    refactor(blocks-construct): resolve sonarqube changes

--- src/features/file-manager/utils/normalize-details-file.ts
+++ src/features/file-manager/utils/normalize-details-file.ts
@@ -3,9 +3,15 @@ import { WithLastModified } from '../types/file-manager.type';
 export const normalizeDetailsFile = <T extends WithLastModified>(file: T | null) => {
   if (!file) return null;
   const lm = file.lastModified;
+  let lastModifiedStr = '';
+  if (typeof lm === 'string') {
+    lastModifiedStr = lm;
+  } else if (lm instanceof Date) {
+    lastModifiedStr = lm.toISOString();
+  }
   return {
     ...file,
-    lastModified: typeof lm === 'string' ? lm : lm instanceof Date ? lm.toISOString() : '',
+    lastModified: lastModifiedStr,
     isShared: file.isShared ?? false,
   } as T & { lastModified: string; isShared: boolean };
 };
